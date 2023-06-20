44eb7260b7980a87aa3d191e934d06c3ba354426
Joffreynk <joyjoffrey@gmail.com>
2023-06-20 20:18:54 +0200

    add title and sub title

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import { Hero } from '@/components'
-import Image from 'next/image'
 
 export default function Home() {
   return (
--- components/hero.tsx
+++ components/hero.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>hero</div>
+    <div className='hero'>
+      <div className='flex-1 pt-36 padding-xs'>
+        <h1 className='hero__title'>Buy, sell, or rent a car - quickly and easily</h1>
+        <p className='hero__subtitle'>Streamline your car rental experience with our effortless process</p>
+      </div>
+      
+    </div>
   )
 }
 
