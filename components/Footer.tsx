d254c7c73b5f41e7991aca0556311ef22188f673
Joffreynk <joyjoffrey@gmail.com>
2023-06-21 20:04:14 +0200

    add main part of footer part

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,18 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
+    <footer className='flex flex-col text-blue-100 mt-5 border-t border-gray-100'>
+      <div className='flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10'>
+        <div className='flex flex-col justify-start items-start gap-6'>
+          <Image src='/logo.svg' alt='car store' width={118} height={18} className='object-contain' />
+          <p className='text-base text-gray-700'>Car store {new Date().getFullYear()} <br />
+          All right reserved &copy; </p>
+        </div>
+      </div>
       
-    </div>
+    </footer>
   )
 }
 
