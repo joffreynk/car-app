7e237347e6ccb8aea4f09c19e3a9208b76a634c9
Joffreynk <joyjoffrey@gmail.com>
2023-06-21 07:04:19 +0200

    finalize footer

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -3,11 +3,11 @@
 import { CustomButtonProps } from '@/types'
 import React from 'react'
 
-const CustomButton = ({title, handleClick, containerStyles}: CustomButtonProps) => {
+const CustomButton = ({title, handleClick, btnType, containerStyles}: CustomButtonProps) => {
   return (
     <button
       disabled={false}
-      type={`button`}
+      type={btnType || `button`}
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
--- types/index.tsx
+++ types/index.tsx
@@ -3,5 +3,6 @@ import { MouseEventHandler } from "react";
 export interface CustomButtonProps {
     title: string;
     containerStyles?: string;
+    btnType?: 'button' | 'submit';
     handleClick?: MouseEventHandler<HTMLButtonElement >;
 }
