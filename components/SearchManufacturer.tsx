de801279ff9ff40b710a512588daec379b8a537b
Joffreynk <joyjoffrey@gmail.com>
2023-06-22 19:47:44 +0200

    add a combobox

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,10 +1,25 @@
+'use client'
+
 import { setManufacturerProps } from '@/types'
+import { Combobox } from '@headlessui/react'
+import Image from 'next/image'
 import React from 'react'
 
 const SearchManufacturer = ({manufacturer, setManufacturer}: setManufacturerProps) => {
   return (
     <div className='search-search-manufacturer'>
-      
+      <Combobox>
+        <div className='relative w-full'>
+          <Combobox.Button className='absolute top-[14px]'>
+            <Image
+            src='/car-logo.svg'
+            width={20}
+            height={20}
+            className='ml-4'
+            alt='car logo' />
+          </Combobox.Button>
+        </div>
+      </Combobox>
     </div>
   )
 }
