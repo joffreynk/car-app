47284ea0313d1549adcbf50f3235cb60482231d3
Joffreynk <joyjoffrey@gmail.com>
2023-06-24 20:58:02 +0200

    add api credentials

---
+++ APICredentials/index.ts
@@ -0,0 +1,9 @@
+export const options = {
+  method: 'GET',
+  url: 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',
+  params: {model: 'corolla'},
+  headers: {
+    'X-RapidAPI-Key': 'bcc661b622msh19966babd8b5fd6p105bd8jsn5e916f4454a1',
+    'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+  }
+};
