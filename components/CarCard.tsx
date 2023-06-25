1f5b34d91b4a8e8f9cb7e84f646fbc50c54e2310
Joffreynk <joyjoffrey@gmail.com>
2023-06-25 13:41:05 +0200

    add car model

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -9,16 +9,23 @@ const CarCard = ({car}: CarCardProps) => {
     const { city_mpg, combination_mpg, cylinders, displacement, drive, fuel_type, highway_mpg, make, model, transmission, year}  = car;
     const carRent  = calculateCarRent(city_mpg, year)
   return (
-    <div className="card-card group">
-      <div className="card-card__content">
-        <h2 className="card-card__content-title">{make} {model} </h2>
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">{make} {model} </h2>
       </div>
       <p className="flex mt-6 text-[24px] font-extrabold">
         <span className="self-start text-[14px] font-semibold">$</span>
         {carRent}
         <span className="self-end text-[14px] font-semibold">/day</span>
       </p>
-      <div></div>
+      <div className="relative w-full my-3 object-contain">
+        <Image src='/public/hero.png' alt="car model" fill priority className="object-contain" />
+      </div>
+      <div className="relative flex w-full mt-2">
+        <div className="flex group-hover:invisible w-full justify-between text-gray">
+
+        </div>
+      </div>
     </div>
   )
 }
