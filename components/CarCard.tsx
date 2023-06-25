837252db60b345737af11071f02df28280e03c68
Joffreynk <joyjoffrey@gmail.com>
2023-06-25 18:37:49 +0200

    add car custom Button

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -7,7 +7,11 @@ import { calculateCarRent } from "@/utils"
 
 const CarCard = ({car}: CarCardProps) => {
     const { city_mpg, combination_mpg, cylinders, displacement, drive, fuel_type, highway_mpg, make, model, transmission, year}  = car;
-    const carRent  = calculateCarRent(city_mpg, year)
+
+    const carRent  = calculateCarRent(city_mpg, year);
+
+    const [isOpen, setIsOpen] = useState(false)
+
   return (
     <div className="car-card group">
       <div className="car-card__content">
@@ -29,6 +33,30 @@ const CarCard = ({car}: CarCardProps) => {
               {transmission === 'a'? 'Automatic' : 'Manual' }
             </p>
           </div>
+
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/tire.svg" width={20} height={20} alt="tire" />
+            <p className="text-[14px]">
+              {drive.toUpperCase()}
+            </p>
+          </div>
+
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/gas.svg" width={20} height={20} alt="gas" />
+            <p className="text-[14px]">
+              {city_mpg } MPG
+            </p>
+          </div>
+        </div>
+        <div className="car-card__btn-cointainer">
+          <CustomButton
+            title="View Moree"
+            containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+            textStyles = "text-white text-[14  leading-[17px] font-bold"
+            rightIcon = "/right-arrow.svg"
+            handleClick={()=>setIsOpen(true)}
+
+          />
         </div>
       </div>
     </div>
