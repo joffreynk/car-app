37a16f0d07f4c779c1a82168d4ac0b8be655569f
Joffreynk <joyjoffrey@gmail.com>
2023-06-25 08:48:52 +0200

    add fetch method

---
+++ utils/index.ts
@@ -0,0 +1,15 @@
+const FetchCars = async () =>{
+  const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars';
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'bcc661b622msh19966babd8b5fd6p105bd8jsn5e916f4454a1',
+      'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+    }
+  };
+
+  const response = await fetch(url, options)
+
+  const result = await response.json()
+  return result
+}
