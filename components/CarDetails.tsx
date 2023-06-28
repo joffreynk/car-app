5019566390e0fc58b512152bffd620e7c2fb5e6d
Joffreynk <joyjoffrey@gmail.com>
2023-06-28 10:53:00 +0200

    add car details model

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -3,15 +3,28 @@
 import { CarDetailsProps } from '@/types'
 import React, {Fragment} from 'react'
 import { Transition, Dialog } from '@headlessui/react'
+import Image from 'next/image'
 
 const CarDetails = ({isOpen, closeModel, car}: CarDetailsProps) => {
   return (
     <>
       <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as='div' className='relative z-10' onClose={closeModel} >
+        <Dialog as='div' className='relative z-20' onClose={closeModel} >
             <Transition.Child as={Fragment} enter='ease-out duration-300' enterFrom='opacity-0' enterTo='opacity-100' leave='ease-in duration-200' leaveFrom='opacity-100' leaveTo='opacity-0'>
               <div className='fixed insert-0 bg-black bg-opacity-25' />
             </Transition.Child>
+            <div className='fixed insert-0 overflow-auto'>
+              <div className='flex min-h-full items-center justify-center p-4 text-center'>
+              <Transition.Child as={Fragment} enter='ease-out duration-300' enterFrom='opacity-0 scale-95' enterTo='opacity-100  scale-100' leave='ease-in duration-200' leaveFrom='opacity-100  scale-100' leaveTo='opacity-0  scale-95'>
+                <Dialog.Panel className='relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white text-left shodow-xsl transition-all flex flex-col gap-5'>
+                  <button type="button" onClick={closeModel} className=' absolute top-2 right-2 z-20 w-fit p-2 bg-primary-blue-100 rounded-full'>
+                    <Image src='/close.svg' alt='Close' width={20} height={20} className='object-contain' />
+                  </button>
+                  <div className='flex-1 w-full h-40 bg-pattern bg-cover bg-center rounded-lg'></div>
+                </Dialog.Panel>
+            </Transition.Child>
+              </div>
+            </div>
         </Dialog>
       </Transition>
     </>
