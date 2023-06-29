8b9de9f4a9f1c90af0f341947bd3e8ee0c6b15b4
Joffreynk <joyjoffrey@gmail.com>
2023-06-29 21:59:42 +0200

    add custom filter data

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,9 +1,67 @@
+'use client'
+
 import { CustomFilterProps } from '@/types'
-import React from 'react'
+import { Listbox, Transition } from '@headlessui/react'
+import Image from 'next/image'
+import React, { Fragment, useState } from 'react'
+
 
 const CustomFilter = ({title, options}: CustomFilterProps) => {
+  const [selected, setSelected] = useState(options[0])
   return (
-    <div>CustomFilter</div>
+    <div className='w-fit'>
+       <Listbox value={selected} onChange={setSelected}>
+        <div className=" relative w-fit z-10">
+          <Listbox.Button className="custom-filter__btn">
+            <span className="block truncate">{selected.title}</span>
+            <Image
+              src='/chevron-up-down.svg'
+              width={20}
+              height={20}
+              className='ml-4 object-contain'
+              alt='chevron-up-down'
+             />
+          </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="custom-filter__options">
+              {options.map((option) => (
+                <Listbox.Option
+                  key={option.title}
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                      active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
+                    }`
+                  }
+                  value={option.title}
+                > 
+                  {({ selected }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? 'font-medium' : 'font-normal'
+                        }`}
+                      >
+                        {option.title}
+                      </span>
+                      {selected ? (
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                          {/* <CheckIcon className="h-5 w-5" aria-hidden="true" /> */}
+                        </span>
+                      ) : null}
+                    </>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      </Listbox>
+    </div>
   )
 }
 
